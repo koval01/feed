f75794b46db8fc12f8d1e716b89a38c6e4dd8bfd
Koval Yaroslav <koval@q-writer.com>
2021-10-17 17:40:48 +0300

    Update core.js

--- core.js
+++ core.js
@@ -1,3 +1,19 @@
+function imgError(o) {
+    return (o.onerror = ""), (o.src = background_static__), !0;
+}
+
+function get_suggestions() {
+    $.ajax({
+        url: "https://feedimages.herokuapp.com/random",
+        type: "GET",
+        success: function (result) {
+            let posts = result.posts
+            console.log(posts);
+        },
+        timeout: 29999,
+    });
+}
+
 $().ready(function() {
     console.log("ready!");
 });
