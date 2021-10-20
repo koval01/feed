328fe47c78785e8f909b0ad421a4b2ce581c0400
Koval Yaroslav <koval@q-writer.com>
2021-10-20 20:50:17 +0300

    Update core.js

--- core.js
+++ core.js
@@ -6,48 +6,6 @@ function imgError(o) {
     return (o.onerror = ""), (o.src = background_static__), !0
 }
 
-addStyle("bootstrap", {
-    html: "<div>\n<span data-notify-text></span>\n</div>",
-    classes: {
-        base: {
-            "font-weight": "bold",
-            "padding": "8px 15px 8px 14px",
-            "text-shadow": "0 1px 0 rgba(255, 255, 255, 0.5)",
-            "background-color": "#fcf8e3",
-            "border": "1px solid #fbeed5",
-            "border-radius": "4px",
-            "white-space": "nowrap",
-            "padding-left": "25px",
-            "background-repeat": "no-repeat",
-            "background-position": "3px 7px"
-        },
-        error: {
-            "color": "#B94A48",
-            "background-color": "#F2DEDE",
-            "border-color": "#EED3D7",
-            "background-image": "none",
-        },
-        success: {
-            "color": "#468847",
-            "background-color": "#DFF0D8",
-            "border-color": "#D6E9C6",
-            "background-image": "none",
-        },
-        info: {
-            "color": "#3A87AD",
-            "background-color": "#D9EDF7",
-            "border-color": "#BCE8F1",
-            "background-image": "none",
-        },
-        warn: {
-            "color": "#C09853",
-            "background-color": "#FCF8E3",
-            "border-color": "#FBEED5",
-            "background-image": "none",
-        }
-    }
-});
-
 function request(callback) {
     $.ajax({
         url: "https://endlessness.herokuapp.com/random",
