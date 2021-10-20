9f33177149f8c9873b0f730e214b97d5ac61e213
Koval Yaroslav <koval@q-writer.com>
2021-10-20 18:42:23 +0300

    Update core.js

--- core.js
+++ core.js
@@ -31,15 +31,6 @@ function get_posts() {
                             <div class="card shadow-sm">
                                 <img class="bd-placeholder-img card-img-top" width="100%" role="img" aria-label="Image"
                                     src="https://endlessness.herokuapp.com/image?l=${data[i].img_link}">
-                                <!-- <div class="card-body">
-                                    <div class="d-flex justify-content-between align-items-center">
-                                        <small class="text-muted">
-                                            ❤️ ${data[i].likes_count} | 
-                                            <a href="https://www.instagram.com/p/${data[i].shortcode}/" 
-                                                target="_blank">Go to post</a>
-                                        </small>
-                                    </div>
-                                </div> -->
                             </div>
                         </div>
                     `)
