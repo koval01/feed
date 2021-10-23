3cc45b96c004e57d14441b150c72bd346772b218
Koval Yaroslav <koval@q-writer.com>
2021-10-23 21:18:43 +0300

    Update core.js

--- core.js
+++ core.js
@@ -71,7 +71,7 @@ $().ready(function() {
         let bodyHeight = document.documentElement.scrollHeight;
         let trigger = Math.max(bodyHeight - (scrollPosition + windowSize), 0);
 
-        if (trigger < (25 * 5000) && first_load && !load_freeze) {
+        if (trigger < 250 && first_load && !load_freeze) {
             load_freeze = true, get_posts() }
     });
     
